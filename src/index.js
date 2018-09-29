fa1b5e2d39e5499d138da22b4de33fb21eba13de
Igor North <japangosha@gmail.com>
2018-09-29 15:45:25 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var result = [];
+var 
+a,
+b,
+c,
+bTemporary = [],
+cTemporary = [],
+Discriminant,
+x_1,
+x_2,
+temporary = equation.split('*');
+
+a = temporary[0];
+bTemporary = temporary[1].split('x^2');
+cTemporary = temporary[2].split('x');
+b = bTemporary[1].split(' ').join('');
+c = cTemporary[1].split(' ').join('');
+
+Discriminant = b * b - 4 * a * c;
+
+if (Discriminant < 0) {
+return result; }
+
+else if (Discriminant === 0) {
+x_1 = -b / 2 * a;
+return result.push(x_1); }
+
+else {
+
+x_1 = Math.round((-b + Math.sqrt(Discriminant)) / (2 * a));
+x_2 = Math.round((-b - Math.sqrt(Discriminant)) / (2 * a));
+
+if (x_1 < x_2) {
+result.unshift(x_1, x_2);
 }
+else {
+result.unshift(x_2, x_1);
+}
+}
+return result;
+
+}
+
